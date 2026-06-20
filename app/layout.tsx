0ea8ac37c356c9379a5d3c5217ee945a5aa66477
DevHanza <hansana123dasanayake@gmail.com>
2026-06-20 10:02:38 +0530

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Sora, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+import Header from "@/components/Header";
+
+const sora = Sora({
+  variable: "--font-sora",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Next.js WP Blog",
+  description: "",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${sora.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body suppressHydrationWarning className="min-h-full flex flex-col">
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem eveniet
+        at voluptatum explicabo nihil ipsam minima dolores harum nisi veritatis
+        eos voluptatibus, atque ducimus ex reiciendis delectus sequi
+        reprehenderit dolore, pariatur perspiciatis architecto natus deleniti
+        esse tempore. Mollitia eum commodi temporibus velit sunt similique
+        exercitationem accusantium molestiae ducimus, consectetur adipisci!
+      </p>
+    </div>
+  );
+}
