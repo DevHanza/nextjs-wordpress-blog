65f13a6a2df0df84a7e2e7a28c113db604c53679
DevHanza <hansana123dasanayake@gmail.com>
2026-06-21 15:16:15 +0530

    Add CardAuthor component

---
+++ components/CardAuthor.tsx
@@ -0,0 +1,19 @@
+"use client";
+import Image from "next/image";
+
+function CardAuthor() {
+  return (
+    <div className="flex items-center gap-3">
+      <Image
+        className="h-6 w-6 rounded-sm md:h-7 md:w-7"
+        src={"/profile.png"}
+        width={28}
+        height={28}
+        alt=""
+      />
+      <span className="text-sm">Mary Anthony</span>
+    </div>
+  );
+}
+
+export default CardAuthor;
