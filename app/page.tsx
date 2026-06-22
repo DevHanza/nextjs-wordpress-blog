0fda4184e4245627090aad2dcf1a8c8753dea433
DevHanza <hansana123dasanayake@gmail.com>
2026-06-22 09:25:38 +0530

    fix: Homepage hero, mini blog card grid responsive styles

--- app/page.tsx
+++ app/page.tsx
@@ -10,14 +10,14 @@ export default function Home() {
     <>
       {/*  1  */}
       <section className="section mt-5 md:mt-12">
-        <div className="container flex flex-col-reverse items-center gap-6 sm:flex-row md:gap-12 lg:gap-22">
+        <div className="container flex flex-col-reverse items-center gap-6 md:flex-row md:gap-12 lg:gap-22">
           <div className="flex flex-1 flex-col">
             <Author />
             <Link
               href={"/"}
               className={"my-6 flex flex-col gap-2 md:mt-8 md:mb-11 md:gap-3"}
             >
-              <h3 className="line-clamp-3 overflow-hidden text-2xl leading-snug tracking-tight sm:text-3xl lg:text-4xl">
+              <h3 className="line-clamp-3 overflow-hidden text-2xl leading-snug tracking-tight md:text-3xl lg:text-4xl">
                 Underrated Tools for Creators, But Your Mindset Is Powerful Than
                 You Think
               </h3>
@@ -49,7 +49,7 @@ export default function Home() {
       {/*  2  */}
       <section className="section">
         <div className="container">
-          <div className="gap grid gap-y-12 gap-x-4 md:grid-cols-2 lg:gap-x-16">
+          <div className="gap grid gap-x-4 gap-y-12 md:grid-cols-2 lg:gap-x-16">
             <BlogCardMini />
             <BlogCardMini />
             <BlogCardMini />
