86e3d191a5452284efce43c49d79a6c14d15e6aa
DevHanza <hansana123dasanayake@gmail.com>
2026-06-22 18:59:49 +0530

    optimize: profile image

--- components/Author.tsx
+++ components/Author.tsx
@@ -6,7 +6,7 @@ function Author() {
     <div className="flex items-center gap-3">
       <Image
         className="h-6 w-6 rounded-sm md:h-7 md:w-7"
-        src={"/profile.png"}
+        src={"/profile.jpg"}
         width={28}
         height={28}
         alt=""
