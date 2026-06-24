4524267bae9acf0677acba9a12f49d60fccb9b44
DevHanza <hansana123dasanayake@gmail.com>
2026-06-24 18:15:48 +0530

    feat(single-blog): Integrate API for single blog post page

--- app/p/[slug]/page.tsx
+++ app/p/[slug]/page.tsx
@@ -1,32 +1,70 @@
 import Image from "next/image";
 import Author from "@/components/blog/Author";
 
-function SinglePostPage() {
+import { getPostBySlug } from "@/lib/wordpress/posts";
+import { notFound } from "next/navigation";
+import { Post } from "@/types/post";
+
+interface Props {
+  params: Promise<{ slug: string }>;
+}
+
+async function SinglePostPage({ params }: Props) {
+  const { slug } = await params;
+
+  let post: Post = await getPostBySlug(slug);
+  post = post[0];
+
+  console.log(post);
+
+  if (!post) notFound();
+
+  //
+  const thumb = post?._embedded["wp:featuredmedia"][0].source_url;
+  const thumb_alt = post?._embedded["wp:featuredmedia"]?.[0]?.alt_text;
+
+  console.log(thumb);
+
+  const author = {
+    name: post?._embedded?.author[0].name,
+    photo: post?._embedded.author[0].avatar_urls["48"],
+  };
+
+  const date = new Date(post.date).toLocaleDateString("en-US", {
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+  });
+
   return (
     <>
       <section className="section pt-5 md:pt-12">
         <div className="g-container max-w-4xl">
           <div className="flex items-center gap-3">
             <div className="">
-              <Author />
+              <Author name={author.name} photo={author.photo} />
             </div>
             <span className="hidden gap-3 text-sm text-gray-500 md:flex">
               <span className="">•</span>
-              <span>Oct 19, 2020</span>
+              <span>{date}</span>
               <span>•</span>
-              <span>2 min read</span>
+              <span>{post.read_time} min read</span>
             </span>
           </div>
 
-          <h1 className="mt-6 line-clamp-10 tracking-tight md:mt-8">
-            Underrated Tools for Creators, But Your Mindset Is Powerful Than You
-            Think
-          </h1>
+          <h1
+            dangerouslySetInnerHTML={{
+              __html: post.title.rendered,
+            }}
+            className="mt-6 line-clamp-10 tracking-tight md:mt-8"
+          ></h1>
 
-          <p className="mt-2 line-clamp-2 max-w-3xl overflow-hidden text-base leading-7 text-gray-500 md:mt-4 md:text-lg">
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy.
-          </p>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: post.excerpt.rendered,
+            }}
+            className="mt-2 line-clamp-2 max-w-3xl overflow-hidden text-base leading-7 text-gray-500 md:mt-4 md:text-lg"
+          ></div>
 
           <span className="mt-6 flex gap-3 text-sm text-gray-500 md:hidden">
             <span>Oct 19, 2020</span>
@@ -35,77 +73,22 @@ function SinglePostPage() {
           </span>
 
           <Image
-            src={"/thumbnail.jpg"}
+            src={thumb}
             className="mt-11 h-75 w-full rounded-2xl object-cover md:mt-12 md:h-100"
             width={900}
             height={400}
-            alt=""
+            alt={thumb_alt}
           />
         </div>
       </section>
 
       <section className="">
-        <div className="g-container max-w-3xl">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
-            ultricies sapien id eros ultrices egestas. Cras et placerat mi,
-            auctor volutpat purus. Donec fermentum egestas leo id volutpat.
-            Integer sed ex vitae ante tempus finibus eget quis lectus. Vivamus
-            eget dolor quis arcu porta malesuada. Quisque id blandit dolor.
-            Quisque volutpat lacus at odio sagittis, eget ultricies libero
-            rutrum.
-          </p>
-          <h2>This is a Heading. </h2>{" "}
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
-            ultricies sapien id eros ultrices egestas. Cras et placerat mi,
-            auctor volutpat purus. Donec fermentum egestas leo id volutpat.
-            Integer sed ex vitae ante tempus finibus eget quis lectus. Vivamus
-            eget dolor quis arcu porta malesuada. Quisque id blandit dolor.
-            Quisque volutpat lacus at odio sagittis, eget ultricies libero
-            rutrum.
-          </p>
-          <h3>This is a Heading. </h3>{" "}
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
-            ultricies sapien id eros ultrices egestas. Cras et placerat mi,
-            auctor volutpat purus. Donec fermentum egestas leo id volutpat.
-            Integer sed ex vitae ante tempus finibus eget quis lectus. Vivamus
-            eget dolor quis arcu porta malesuada. Quisque id blandit dolor.
-            Quisque volutpat lacus at odio sagittis, eget ultricies libero
-            rutrum.
-          </p>
-          <h4>This is a Heading. </h4>{" "}
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
-            ultricies sapien id eros ultrices egestas. Cras et placerat mi,
-            auctor volutpat purus. Donec fermentum egestas leo id volutpat.
-            Integer sed ex vitae ante tempus finibus eget quis lectus. Vivamus
-            eget dolor quis arcu porta malesuada. Quisque id blandit dolor.
-            Quisque volutpat lacus at odio sagittis, eget ultricies libero
-            rutrum.
-          </p>
-          <h5>This is a Heading. </h5>{" "}
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
-            ultricies sapien id eros ultrices egestas. Cras et placerat mi,
-            auctor volutpat purus. Donec fermentum egestas leo id volutpat.
-            Integer sed ex vitae ante tempus finibus eget quis lectus. Vivamus
-            eget dolor quis arcu porta malesuada. Quisque id blandit dolor.
-            Quisque volutpat lacus at odio sagittis, eget ultricies libero
-            rutrum.
-          </p>
-          <h6>This is a Heading. </h6>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
-            ultricies sapien id eros ultrices egestas. Cras et placerat mi,
-            auctor volutpat purus. Donec fermentum egestas leo id volutpat.
-            Integer sed ex vitae ante tempus finibus eget quis lectus. Vivamus
-            eget dolor quis arcu porta malesuada. Quisque id blandit dolor.
-            Quisque volutpat lacus at odio sagittis, eget ultricies libero
-            rutrum.
-          </p>
-        </div>
+        <div
+          className="g-container max-w-3xl"
+          dangerouslySetInnerHTML={{
+            __html: post.content?.rendered,
+          }}
+        ></div>
       </section>
     </>
   );
