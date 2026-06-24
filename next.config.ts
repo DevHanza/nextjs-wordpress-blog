af18aa713f3183dcdfb5cc1299b4391711cb2122
DevHanza <hansana123dasanayake@gmail.com>
2026-06-24 13:47:54 +0530

    config (next.config.ts): Add remote patterns for images

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,14 @@ const isDev = process.env.NODE_ENV === "development";
 const nextConfig: NextConfig = {
   images: {
     unoptimized: isDev,
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "8080",
+        pathname: "**",
+      },
+    ],
   },
 };
 
