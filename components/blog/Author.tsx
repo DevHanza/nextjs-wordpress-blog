5d92d5da08e47f7a48768366fb606c9d9e91f4e2
DevHanza <hansana123dasanayake@gmail.com>
2026-06-24 14:53:25 +0530

    feat (global): Add props to Author component

--- components/blog/Author.tsx
+++ components/blog/Author.tsx
@@ -1,17 +1,22 @@
 "use client";
 import Image from "next/image";
 
-function Author() {
+interface Props {
+  name: string;
+  photo: string;
+}
+
+function Author({ name = "Mark Anthony", photo = "/profile.jpg" }: Props) {
   return (
     <div className="flex items-center gap-3">
       <Image
         className="h-6 w-6 rounded-sm md:h-7 md:w-7"
-        src={"/profile.jpg"}
+        src={photo}
         width={28}
         height={28}
         alt=""
       />
-      <span className="line-clamp-1 text-sm">Mary Anthony</span>
+      <span className="line-clamp-1 text-sm">{name}</span>
     </div>
   );
 }
