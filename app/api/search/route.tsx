f63d87300cec056180eda6f7c97fd42f5ceb3c5c
DevHanza <hansana123dasanayake@gmail.com>
2026-06-28 18:48:38 +0530

    fix: (api): added missing args - search posts route

--- app/api/search/route.tsx
+++ app/api/search/route.tsx
@@ -6,11 +6,13 @@ export async function GET(request: NextRequest) {
 
   const query = params.get("q") || "";
   const page = Number(params.get("page") || 1);
+  const per_page = Number(params.get("per_page") || 10);
 
   try {
-    const data = await searchPosts(query, page);
-
+    //
+    const data = await searchPosts(query, page, per_page);
     return NextResponse.json(data);
+    //
   } catch {
     return NextResponse.json({ message: "Failed" }, { status: 500 });
   }
