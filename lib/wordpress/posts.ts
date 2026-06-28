31886821a38840acfc73c0e30b339cac13aba863
DevHanza <hansana123dasanayake@gmail.com>
2026-06-28 13:51:12 +0530

    feat (search): Add searchPosts to post fetching API utility

--- lib/wordpress/posts.ts
+++ lib/wordpress/posts.ts
@@ -1,7 +1,7 @@
 import { fetchAPI } from "./client";
 import { Post } from "@/types/post";
 
-interface PostsResponse {
+export interface PostsResponse {
   posts: Post[];
   totalPages: number;
 }
@@ -23,13 +23,24 @@ export async function getPosts(
   };
 }
 
-// export async function getPosts(page = 1, per_page = 10): Promise<Post[]> {
-//   const res = await fetchAPI(
-//     `wp/v2/posts?_embed&page=${page}&per_page=${per_page}`,
-//   );
-//   return res?.json();
-//   // _fields=id,slug,title,excerpt
-// }
+export async function searchPosts(
+  query: string,
+  page = 1,
+  per_page = 10,
+): Promise<PostsResponse> {
+  const q = encodeURIComponent(query);
+  const res = await fetchAPI(
+    `wp/v2/posts?search=${q}&page=${page}&per_page=${per_page}&_embed`,
+  );
+
+  const posts: Post[] = await res?.json();
+  const totalPages = parseInt(res?.headers.get("x-wp-totalpages") || "0");
+
+  return {
+    posts,
+    totalPages,
+  };
+}
 
 export async function getPostBySlug(slug: string): Promise<Post> {
   const res = await fetchAPI(`wp/v2/posts?slug=${slug}&_embed`);
